75d9872580fdbcc67212afb547210bb99b981b98
Aleksander Wiażewicz <wiazewicz@gmail.com>
2020-02-18 21:05:36 +0100

    modal modif

--- main.js
+++ main.js
@@ -24,10 +24,10 @@ $.ajax({
                 <div class="modal-body">
                     <img src="${user.picture.large}"> <br/>
                     <p><span class="gender-styled">${user.gender}</span>, ${user.dob.age} y.o.</p>
-                    <p><span>Living:</span> ${user.location.postcode}, ${user.location.street.name} ${user.location.street.number}, ${user.location.city}, ${user.location.state}, ${user.location.country} (${user.nat})</p>
-                    <p><span>Currently:</span><iframe class="map" src="https://maps.google.com/maps?q=${user.location.coordinates.latitude},${user.location.coordinates.longitude}&hl=en&z=6&amp;output=embed" frameborder="0" allowfullscreen></iframe>
-                    <p><span>Email:</span><a class="email-link" href="mailto:${user.email}"> ${user.email}</a></p>
-                    <p><span>Cell:</span><a class="phone-link" href="tel:${user.cell}"> ${user.cell}</a></p>                                 
+                    <p><span>Living:</span><a class="my-link" href="https://maps.google.com/?q= ${user.location.city} ${user.location.street.name}"> ${user.location.postcode}, ${user.location.street.name} ${user.location.street.number}, ${user.location.city}, ${user.location.state}, ${user.location.country} (${user.nat})</a></p>
+                    <p><span>Current location:</span><iframe class="map" src="https://maps.google.com/maps?q=${user.location.coordinates.latitude},${user.location.coordinates.longitude}&hl=en&z=6&amp;output=embed" frameborder="0" allowfullscreen></iframe>
+                    <p><span>Email:</span><a class="my-link" href="mailto:${user.email}"> ${user.email}</a></p>
+                    <p><span>Cell:</span><a class="my-link" href="tel:${user.cell}"> ${user.cell}</a></p>                                 
                 </div>
             `);
         });
