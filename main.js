3756ca0de6155d5df70f8a12ff0d0ddb626ad834
Aleksander Wiażewicz <wiazewicz@gmail.com>
2020-02-19 00:23:01 +0100

    email

--- main.js
+++ main.js
@@ -24,10 +24,10 @@ $.ajax({
                 <div class="modal-body">
                     <img src="${user.picture.large}">
                     <p><span>Gender:</span><br/><span class="gender-styled">${user.gender}</span></p>
-                    <p><span>Age:</span><br/>${user.dob.age} y.o.</p><br/>
+                    <p><span>Age:</span><br/>${user.dob.age} y.o.</p>
                     <p><span>Living in:</span><br/><a class="my-link" href="https://maps.google.com/?q= ${user.location.city} ${user.location.street.name}"> ${user.location.postcode}, ${user.location.street.name} ${user.location.street.number}, ${user.location.city}, ${user.location.state}, ${user.location.country} (${user.nat})</a></p>
                     <p><span>Current location:</span><iframe class="map" src="https://maps.google.com/maps?q=${user.location.coordinates.latitude},${user.location.coordinates.longitude}&hl=en&z=1&amp;output=embed" frameborder="0" allowfullscreen></iframe>
-                    <p><span>Email:</span><a class="my-link" href="mailto:${user.email}"> ${user.email}</a></p>
+                    <p><span>Email:</span><a class="my-link" href="mailto:${user.email}?subject=${user.name.first}! I've found You on Love Finder <3&body=Hi ${user.name.first}, I think we could get to know each other better. Reply to this email if you are interested in a new friendship."> ${user.email}</a></p>
                     <p><span>Cell:</span><a class="my-link" href="tel:${user.cell}"> ${user.cell}</a></p>                                 
                 </div>
             `);
