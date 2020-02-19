159f8543da88164aa80322c311f568fb2c230cba
Aleksander Wiażewicz <wiazewicz@gmail.com>
2020-02-19 09:39:38 +0100

    gender icons

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ $.ajax({
     datatype: 'json',
     method: 'GET',
     success: function(data){
-        console.log(data);
+        // console.log(data);
         data.results.forEach(function(user) {
             // debugger;
             $('.main').html($(`
@@ -18,17 +18,17 @@ $.ajax({
 
             $('#myModal').find('.modal-content').html(`
                 <div class="modal-header">
-                    <h3>${user.name.title} ${user.name.first} ${user.name.last}</h3>
+                    <h6>${user.name.title}</h6><h4>${user.name.first} ${user.name.last}</h4>
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
                 </div>
                 <div class="modal-body">
                     <img src="${user.picture.large}">
-                    <p><span>Gender:</span><br/><span class="gender-styled">${user.gender}</span></p>
+                    <p id="gender-span"><span>Gender:</span><br/><span id="updated-gender" class="gender-styled">${user.gender}</span></p>
                     <p><span>Age:</span><br/>${user.dob.age} y.o.</p>
                     <p><span>Living in:</span><br/><a class="my-link" href="https://maps.google.com/?q= ${user.location.city} ${user.location.street.name}"> ${user.location.postcode}, ${user.location.street.name} ${user.location.street.number}, ${user.location.city}, ${user.location.state}, ${user.location.country} (${user.nat})</a></p>
                     <p><span>Current location:</span><iframe class="map" src="https://maps.google.com/maps?q=${user.location.coordinates.latitude},${user.location.coordinates.longitude}&hl=en&z=1&amp;output=embed" frameborder="0" allowfullscreen></iframe>
                     <p><span>Email:</span><a class="my-link" href="mailto:${user.email}?subject=${user.name.first}! I've found You on Love Finder <3&body=Hi ${user.name.first}, I think we could get to know each other better. Reply to this email if you are interested in a new friendship."> ${user.email}</a></p>
-                    <p><span>Cell:</span><a class="my-link" href="tel:${user.cell}"> ${user.cell}</a></p>                                 
+                    <p class="last-p"><span>Cell:</span><a class="my-link" href="tel:${user.cell}"> ${user.cell}</a></p>                                 
                 </div>
             `);
         });
@@ -41,3 +41,13 @@ $.ajax({
         $('.decide').remove();
     }
 });
+
+function genderIcons() {
+    if ($('#updated-gender').text() == 'male') {
+        $('#gender-span').append(' <span class="fas fa-mars"></span>');
+    } else {
+        $('#gender-span').append(' <span class="fas fa-venus"></span>');
+    }
+};
+
+$('.fa-user-check').on('click', genderIcons);
